1f6c6e4ac7e407dd7c68d6ba377f4cd330d9a4bd
Marcelo <marcelosen4@gmail.com>
2021-03-12 17:20:57 -0300

    Se creo la app de todo con dos items del menu: Crear tarea y obtener todas las tareas

--- 04-node-todo-app/index.js
+++ 04-node-todo-app/index.js
@@ -1,5 +1,31 @@
+const { inquirerMenu, inquirerInput } = require('./helpers/inquirer');
+const TaskRepository = require('./repositories/TaskRepository');
+
 const main = async () => {
-    console.log('Hola Mundo!');
+
+    const taskRepository = new TaskRepository();
+    let option = '';
+
+    do {
+        option = await inquirerMenu();
+
+        switch (option) {
+            case 1:
+    
+                console.log('El usuario selecciono la opcion de crear tarea');
+                const title = await inquirerInput('Task Title');
+                taskRepository.createTask(title);
+                break;
+            
+            case 2:
+    
+                const allTasks = taskRepository.getAllTask();
+                console.log(allTasks);
+                break;
+        }
+
+    } while (option !== 0);
+
 }
 
 main();
--- 04-node-todo-app/repositories/TaskRepository.js
+++ 04-node-todo-app/repositories/TaskRepository.js
@@ -1,20 +1,27 @@
 const Task = require('../models/Task');
+const { getData, saveData } = require ('../helpers/fileManager');
 
 class TaskRepository {
 
     _tasks = null;
     
     constructor() {
-        this._tasks = [];
+        const data = getData();
+        if(data) {
+            this._tasks = data
+        } else {
+            this._tasks = [];
+        }
     }
     
     getAllTask() {
-
+        return this._tasks;
     }
 
     createTask(title) {
         const task = new Task(title);
         this._tasks.push(task);
+        saveData(this._tasks);
     }
 
     deleTask() {
@@ -25,3 +32,5 @@ class TaskRepository {
 
     }
 }
+
+module.exports = TaskRepository;
